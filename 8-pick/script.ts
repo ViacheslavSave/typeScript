03393d9da11ce44ef5f7bccff31ecd04c7e511e6
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2024-01-29 21:42:14 +0300

    изменения после кодревью

--- 8-pick/script.ts
+++ 8-pick/script.ts
@@ -1,39 +1,16 @@
-interface IUser {
-	[prop: string | symbol]: unknown;
-}
-
 const user = {
 	name: "vasiliy",
 	age: 8,
 	skills: ["typescript", "javascript"],
 };
 
-function pickObjectKeys<T extends IUser>(user: T, prop: (keyof T)[]): IUser {
-	return prop.reduce<IUser>((acc, elem) => {
+function pickObjectKeys<T extends E, K extends keyof T, E extends Pick<T, K>>(user: T, prop: K[]): E {
+	return prop.reduce((acc, elem) => {
 		acc[elem] = user[elem];
 		return acc;
-	}, {});
+	}, {} as E);
 }
-// работает
-const res = pickObjectKeys(user, ["name", "age"]);
 
-// работает,нет ошибки      как можно ограничить в ts использование дубликатов ?
+const res1 = pickObjectKeys(user, ["name", "age"]);
+
 const res2 = pickObjectKeys(user, ["name", "age", "age"]);
-console.log(res);
-
-//----------------------------------------------------------------------------
-
-const arr = ["age", "name"] as const;
-
-// как в функции можно получить такой тип  unionType  если он приходит в параметры функции ?
-//       ↓
-type unionType = (typeof arr)[number];
-
-type newTypeUser = getNewType<typeof user, unionType>;
-
-type getNewType<T, E extends keyof T> = {
-	[prop in keyof T as Extract<prop, E >]: T[prop];
-  // [P in Extract<keyof T, E>]: T[P]; 
-};
-
-export{}
